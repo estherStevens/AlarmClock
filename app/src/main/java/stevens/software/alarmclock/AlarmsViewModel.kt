65a3e8e852a09d486ac465c6820d8b3e05e9fe8a
Esther Stevens <estherstevens@Esthers-MacBook-Pro.local>
2024-11-07 07:45:55 +0000

    build alarms screen with mock data

---
+++ app/src/main/java/stevens/software/alarmclock/AlarmsViewModel.kt
@@ -0,0 +1,35 @@
+package stevens.software.alarmclock
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class AlarmsViewModel: ViewModel() {
+
+    val uiState = MutableStateFlow<AlarmsUiState>(
+        AlarmsUiState(
+            alarms = mockAlarms()
+        )
+    )
+
+    fun mockAlarms() = listOf<Alarm>(
+        Alarm(
+            time = "10:00",
+            selectedDays = listOf(SelectedDaysOfTheWeek.MONDAY, SelectedDaysOfTheWeek.TUESDAY)
+        ),
+        Alarm(
+            time ="13:00",
+            selectedDays = listOf(SelectedDaysOfTheWeek.MONDAY, SelectedDaysOfTheWeek.TUESDAY)
+        )
+    )
+}
+
+data class AlarmsUiState(val alarms: List<Alarm>)
+
+data class Alarm(
+    val time: String,
+    val selectedDays: List<SelectedDaysOfTheWeek>
+)
+
+
+
+enum class SelectedDaysOfTheWeek { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY}
